bf2aac62b4ee719f4929b1ca85034ffdb6617aee
coldcloude <coldcloude@163.com>
2025-03-05 14:48:03 +0800

    新增realtime broker，部分功能改为异步

--- src/__test__/test.ts
+++ src/__test__/test.ts
@@ -10,3 +10,31 @@ import { testRoute } from "./test-route.js";
 
 console.log(">>>>>>>>>>>>>>>>test route");
 testRoute();
+
+function ax(op:(r:number)=>void){
+    console.log("ax");
+    op(1);
+}
+
+async function xx():Promise<number>{
+    console.log("xx");
+    return 1;
+}
+
+function axx():Promise<number>{
+    console.log("axx");
+    return new Promise<number>((cb)=>ax(cb));
+}
+
+console.log("init");
+
+const y = xx();
+
+const ay = axx();
+
+console.log("await");
+
+await y;
+await ay;
+
+console.log("end");
